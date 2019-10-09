5cc19d73ba2dc5714b8d0d7b6aecc4a36ad2c868
roman120487 <roman120487@ukr.net>
2019-10-09 23:51:48 +0300

    start

---
+++ src/app/data/data.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data',
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.css']
+})
+export class DataComponent implements OnInit {
+  persons: Array<any>;
+  id: number;
+  FName: string;
+  LName: string;
+  phone: number;
+
+  constructor() {
+    this.persons = [
+      {
+        id: 1,
+        FName: 'Roman',
+        LName: 'Kostiv',
+        phone: '0979455318'
+      },
+      {
+        id: 2,
+        FName: 'Vasya',
+        LName: 'Lipkovskiy',
+        phone: '0995553336'
+      },
+      {
+        id: 3,
+        FName: 'Sveta',
+        LName: 'Kukurudza',
+        phone: '0975456736'
+      },
+      {
+        id: 4,
+        FName: 'Petro',
+        LName: 'Pisun',
+        phone: '09123456789'
+      }
+    ];
+  }
+
+  ngOnInit() {
+  }
+
+  addPhone(): void {
+    const newPerson: IPerson = new Person(3, this.FName, this.LName, this.phone);
+    newPerson.id = this.persons.slice(-1)[0].id + 1;
+    this.persons.push(newPerson)
+    console.log(this.persons);
+    
+  }
+
+}
+
+interface IPerson {
+  id: number;
+  FName: string;
+  LName: string;
+  phone: number;
+}
+
+class Person implements IPerson {
+  constructor(
+    public id: number,
+    public FName: string,
+    public LName: string,
+    public phone: number
+  ) { }
+}
+
