1f5701d90a3f6485ef4f09291d6030b70f3dc9ab
roman120487 <roman120487@ukr.net>
2019-10-15 13:26:00 +0300

    finish

--- src/app/shared/pipes/name.pipe.ts
+++ src/app/shared/pipes/name.pipe.ts
@@ -8,7 +8,17 @@ export class NamePipe implements PipeTransform {
   transform(value: any[], search: string): any {
     if (!value) { return []; }
     if (!search) { return value; }
-    return value.filter(val => val.FName.indexOf(search) != -1);
+    return value.filter(function (val) {
+      if (val.FName.indexOf(search) != -1) {
+        return val;
+      }
+      if (val.LName.indexOf(search) != -1) {
+        return val;
+      }
+      if (val.phone.indexOf(search) != -1) {
+        return val;
+      }
+    });
   }
 
 }
